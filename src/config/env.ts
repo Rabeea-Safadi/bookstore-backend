7addfeaf40b9ad875c023af0b18cc3aa62947a95
Rabeea Safadi <rabeeasafadi@gmail.com>
2022-11-29 16:40:07 +0200

    if db uri is missing it defaults to local mongodb

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,4 +3,4 @@ import { config } from 'dotenv';
 config();
 
 export const PORT = process.env.PORT || 3030;
-export const DATABASE_URI = process.env.DATABASE_URI || null;
+export const DATABASE_URI = process.env.DATABASE_URI || 'mongodb://127.0.0.1:27017/bookstore';
